20ee5c9ddef31d339b27af7f89bb1d148e4b9ebc
Bryce Tichit <tichit.bryce@gmail.com>
2021-01-27 09:51:12 +0000

    Init commit

---
+++ src/features/events/eventForm/EventForm.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button, Form, Header, Segment } from 'semantic-ui-react';
+
+export default function EventForm({setFormOpen}) {
+    return (
+        <Segment clearing>
+            <Header content='Create new event' />
+            <Form>
+                <Form.Field>
+                    <input type='text' placeholder='Event title' />
+                </Form.Field>
+                <Form.Field>
+                    <input type='text' placeholder='Description' />
+                </Form.Field>
+                <Form.Field>
+                    <input type='text' placeholder='City' />
+                </Form.Field>
+                <Form.Field>
+                    <input type='text' placeholder='Venue' />
+                </Form.Field>
+                <Form.Field>
+                    <input type='date'/>
+                </Form.Field>
+                <Button type='submit' floated='right' positive content='Submit' />
+                <Button onClick={() => setFormOpen(false)} type='submit' floated='right' content='Cancel' />
+            </Form>
+        </Segment>
+    )
+}
