5ff7a94c5fcd4cff529db101a6fca5e47b680acf
Bryce Tichit <tichit.bryce@gmail.com>
2021-03-02 23:07:26 +0000

    Ensure that we do cannot run the firestore functions if the user is not logged in

--- src/app/firestore/firebaseService.js
+++ src/app/firestore/firebaseService.js
@@ -11,6 +11,14 @@ export function signOutFirebase() {
     return firebase.auth().signOut()
 }
 
+export async function getToken() {
+    const user = firebase.auth().currentUser;
+
+    if(!user)
+        throw new Error("User not logged in.")
+    return await user.getIdToken(true)
+}
+
 export async function registerInFirebase(creds) {
     try {
         const result = await firebase.auth().createUserWithEmailAndPassword(creds.email, creds.password)
@@ -26,6 +34,8 @@ export async function registerInFirebase(creds) {
 
 export function updateUserPassword(creds) {
     const user = firebase.auth().currentUser;
+    if(!user)
+        throw new Error("User not logged in.")
     return user.updatePassword(creds.newPassword1)
 }
 
@@ -34,6 +44,8 @@ export function uploadBookDataToFirebaseStore(bookId, bookPdfFile) {
     const filename_pdf = bookId + '.pdf'
 
     const user = firebase.auth().currentUser
+    if(!user)
+        throw new Error("User not logged in.")
     const storageRef = firebase.storage().ref()
     const pdfUploadTask = storageRef
         .child(`${user.uid}/userBooks/${bookId}/${filename_pdf}`)
