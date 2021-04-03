2f398310a6df3efb6df40fa03695abfb8fb116e2
Bryce Tichit <tichit.bryce@gmail.com>
2021-04-03 14:29:34 +0100

    sandbox

--- src/features/sandbox/Sandbox.jsx
+++ src/features/sandbox/Sandbox.jsx
@@ -1,42 +1,58 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Button } from 'semantic-ui-react';
-import { openModal } from '../../app/common/modals/modalReducer';
-import { callBackendTest } from './backend';
-import { decrement, increment} from './testReducer';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Button } from "semantic-ui-react";
+import { openModal } from "../../app/common/modals/modalReducer";
+import { useFirestoreCollectionOnce } from "../../app/hooks/useFirestoreCollection";
+import { getBooksMetadataFromFirestore } from "../../app/firestore/firestoreService"
+import { callBackendTest } from "./backend";
+import { decrement, increment } from "./testReducer";
+import GKeepList from "../../app/common/GKeepList/GKeepList";
 
 export default function Sandbox() {
-    const dispatch = useDispatch();
-    const [target, setTarget] = useState(null);
-    const data = useSelector(state => state.test.data)
-    const {loading} = useSelector(state => state.async)
+  const dispatch = useDispatch();
+  const [target, setTarget] = useState(null);
+  const data = useSelector((state) => state.test.data);
+  const { loading } = useSelector((state) => state.async);
 
-    callBackendTest();
+  const initCoucou = "<ul><li/></ul>"
+  const [coucou, setCoucou] = useState(initCoucou)
 
-    return (
-        <>
-            {/* Your token is: {getToken()} */}
-            <h1>Test 123</h1>
-            <h3>The data is: {data}</h3>
-            <Button
-                name='increment'
-                loading={loading && target === 'decrement'} onClick={(e) => {
-                  dispatch(increment(20))
-                  setTarget(e.target.name)
-                }
-                }
-                 content='Increment' color='green'/>
-            <Button
-                name='decrement'
-                loading={loading && target === 'decrement'}
-                onClick={(e) => {
-                   dispatch(decrement(10));
-                   setTarget(e.target.name)
-                } }
-                content='Decrement' color='orange'/>
-            <Button onClick={() => dispatch(openModal({modalType: 'TestModal', modalProps: {data}}))} content='Open Modal' color='teal'/>
+  callBackendTest();
 
-            Mode: {'_self' in React.createElement('div') ? "Dev" : "Prod"}
-        </>
-    )
+  return (
+    <>
+      {/* Your token is: {getToken()} */}
+      <h1>Test 123</h1>
+      <h3>The data is: {data}</h3>
+      <Button
+        name="increment"
+        loading={loading && target === "decrement"}
+        onClick={(e) => {
+          dispatch(increment(20));
+          setTarget(e.target.name);
+        }}
+        content="Increment"
+        color="green"
+      />
+      <Button
+        name="decrement"
+        loading={loading && target === "decrement"}
+        onClick={(e) => {
+          dispatch(decrement(10));
+          setTarget(e.target.name);
+        }}
+        content="Decrement"
+        color="orange"
+      />
+      <Button
+        onClick={() =>
+          dispatch(openModal({ modalType: "TestModal", modalProps: { data } }))
+        }
+        content="Open Modal"
+        color="teal"
+      />
+      Mode: {"_self" in React.createElement("div") ? "Dev" : "Prod"}
+    <GKeepList/>
+    </>
+  );
 }
