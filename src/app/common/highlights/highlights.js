e3f883dafa5262fd677e3e1b58807385706db123
Bryce Tichit <tichit.bryce@gmail.com>
2021-04-03 14:21:14 +0100

    Firestore functions to update highlights and highlights metadata

--- src/app/common/highlights/highlights.js
+++ src/app/common/highlights/highlights.js
@@ -1,16 +1,19 @@
 import * as R from "ramda";
-import { updateHighlightsInFirestore } from "../../firestore/firestoreService";
+import { reduceEachTrailingCommentRange } from "typescript";
+import { updateHighlightsInFirestore, updateHighlightsMetadataInFirestore } from "../../firestore/firestoreService";
 
 const getNextId = () => String(Math.random()).slice(2);
 
 export async function deleteHighlight(highlights, bookId, highlightId) {
   const new_highlights = highlights.filter((e) => e.id !== highlightId);
   await updateHighlightsInFirestore(bookId, new_highlights);
+  await updateHighlightsMetadataInFirestore(bookId, new_highlights)
 }
 
 export async function addHighlight(highlights, highlight, bookId) {
   const new_highlights = [{ ...highlight, id: getNextId() }, ...highlights];
   await updateHighlightsInFirestore(bookId, new_highlights);
+  await updateHighlightsMetadataInFirestore(bookId, new_highlights)
 }
 
 export async function sortHighlights(highlights, bookId) {
@@ -22,16 +25,19 @@ export async function sortHighlights(highlights, bookId) {
   await updateHighlightsInFirestore(bookId, new_highlights);
 }
 
-export async function updateHighlight(highlights, highlightId, position, content, bookId) {
-  console.log("Updating highlight", highlightId, position, content);
+export async function updateHighlight(highlights, highlightId, bookId, {position={}, content={}, comment={}}) {
+  console.log("Updating highlight", highlightId, {position, content, comment});
+
+  if (!position && !content && !comment) return;
 
   const new_highlights = highlights.map((h) => {
-    const { id, originalPosition, originalContent, ...rest } = h;
+    const { id, position: originalPosition, content: originalContent, comment: originalComment, ...rest } = h;
     return id === highlightId
       ? {
           id,
           position: { ...originalPosition, ...position },
           content: { ...originalContent, ...content },
+          comment: {...originalComment, ...comment},
           ...rest,
         }
       : h;
