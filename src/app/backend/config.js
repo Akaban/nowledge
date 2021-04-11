58b5f7f0bf63532d30e16b77bca07be1f3aec012
Bryce Tichit <tichit.bryce@gmail.com>
2021-04-11 16:26:39 +0100

    Archive running tasks when an async error is encountered

--- src/app/backend/config.js
+++ src/app/backend/config.js
@@ -1,5 +1,3 @@
 import { isReactDevMode } from "../common/util/util"
 
 export const API_URL = isReactDevMode() ? "http://localhost:5000/api/v1" : "https://backend.nowledge.xyz/api/v1"
-
-console.log(API_URL)
