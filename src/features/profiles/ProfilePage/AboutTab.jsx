c6f37af64d705e576d70e274bb9acaee38064732
Bryce Tichit <tichit.bryce@gmail.com>
2021-04-20 14:58:15 +0100

    Update profile page

--- src/features/profiles/ProfilePage/AboutTab.jsx
+++ src/features/profiles/ProfilePage/AboutTab.jsx
@@ -1,56 +1,71 @@
-import React, { useState } from 'react'
-import { Redirect } from "react-router-dom"
-import { Container, Grid, Header, Tab } from 'semantic-ui-react'
-import { format } from 'date-fns'
-import ProfileForm from './ProfileForm.oldjsx'
-import useAsyncEffect from '../../../app/hooks/useAsyncEffect'
-import { getPremiumInfo } from '../../../app/backend/premium'
-import { useSelector, useDispatch } from "react-redux"
-import LoadingComponent from '../../../app/layout/LoadingComponents'
-import { ProgressBar } from 'react-bootstrap'
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import { Container, Grid, Header, Tab } from "semantic-ui-react";
+import { format } from "date-fns";
+import ProfileForm from "./ProfileForm.oldjsx";
+import useAsyncEffect from "../../../app/hooks/useAsyncEffect";
+import { getPremiumInfo } from "../../../app/backend/premium";
+import { useSelector, useDispatch } from "react-redux";
+import LoadingComponent from "../../../app/layout/LoadingComponents";
+import { ProgressBar } from "react-bootstrap";
+import useFirestoreDoc from "../../../app/hooks/useFirestoreDoc";
+import { getUserPlan } from "../../../app/firestore/firestoreService";
+import { listentoCurrentUserPlan } from "../profileActions";
 
-export default function AboutTab({profile, userPlan}) {
-    const dispatch = useDispatch();
-    String.prototype.capitalize = function() {
-  return this.charAt(0).toUpperCase() + this.slice(1)
-}
+export default function AboutTab({ profile }) {
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.auth);
+  const current_user_id = currentUser.id;
+  String.prototype.capitalize = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  };
+
+  const { userPlan } = useSelector((state) => state.profile);
+
 
   const { error, loading } = useSelector((state) => state.async);
-  const [premiumInfo, setPremiumInfo] = useState(null);
-  useAsyncEffect({
-    fetch: () => getPremiumInfo(),
-    after: setPremiumInfo,
-    name: "getPremiumInfo",
-    deps: [dispatch]
+  useFirestoreDoc({
+    query: () => getUserPlan(current_user_id),
+    data: (plan) => dispatch(listentoCurrentUserPlan(plan)),
+    deps: [dispatch, current_user_id],
+    name: "getUserPlan",
+    silent: true,
   });
-  if (loading && (!premiumInfo) && !error)
-    return <LoadingComponent content="Loading..." />;
+  if (!userPlan && !error) return <LoadingComponent content="Loading..." />;
   if (error) return <Redirect to="/error" />;
-  const progressStorageSize = Math.round((premiumInfo.current_usage.books_storage_size / premiumInfo.user_limits.max_space) * 100)
-  const progressBookCount = Math.round((premiumInfo.current_usage.books_count / premiumInfo.user_limits.max_books) * 100)
+  const max_space = userPlan.plan === "basic" ? 1e9 : 30e6
+
+  console.log(userPlan)
+
+  const progressStorageSize = Math.round(
+    (userPlan.user_books_storage_size /
+      max_space) *
+      100
+  );
 
   const get_variant_from_progress = (progress) => {
-      if (progress > 85) return "danger";
-      else return "info";
-  }
-    return (
-        <Tab.Pane>
-        <Grid>
-            <Grid.Column width={16}>
-                <Header floated='left' icon='user' content={`Plan`} />
-                {/* <Button onClick={() => setEditMode(!editMode)} floated='right' basic content={editMode ? 'Cancel': 'Edit'} /> */}
-            </Grid.Column>
-            <Grid.Column width={16}>
-                <strong>Current plan:</strong> {userPlan.plan.capitalize()}
-                <Header content="Current limits"/>
-        Storage usage: <ProgressBar striped variant={get_variant_from_progress(progressStorageSize)} now={progressStorageSize} label={`${progressStorageSize}%`}  />
-        
-        {premiumInfo.user_limits.max_books &&
-        <ProgressBar striped variant={get_variant_from_progress(progressBookCount)} now={progressBookCount} label={`${progressBookCount}%`}  />
-        }
-            </Grid.Column>
-        </Grid>
-
-        </Tab.Pane>
-    )
+    if (progress > 85) return "danger";
+    else return "info";
+  };
+  return (
+    <Tab.Pane>
+      <Grid>
+        <Grid.Column width={16}>
+          <Header floated="left" icon="user" content={`Plan`} />
+          {/* <Button onClick={() => setEditMode(!editMode)} floated='right' basic content={editMode ? 'Cancel': 'Edit'} /> */}
+        </Grid.Column>
+        <Grid.Column width={16}>
+          <strong>Current plan:</strong> {userPlan.plan.capitalize()}
+          <Header content="Current limits" />
+          Storage usage:{" "}
+          <ProgressBar
+            striped
+            variant={get_variant_from_progress(progressStorageSize)}
+            now={progressStorageSize}
+            label={`${progressStorageSize}%`}
+          />
+        </Grid.Column>
+      </Grid>
+    </Tab.Pane>
+  );
 }
--- src/features/profiles/ProfilePage/ProfileContent.jsx
+++ src/features/profiles/ProfilePage/ProfileContent.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import { Tab } from 'semantic-ui-react'
-import AboutTab from './AboutTab'
-import PasswordForm from './PasswordForm'
+import React from "react";
+import { Tab } from "semantic-ui-react";
+import AboutTab from "./AboutTab";
+import PasswordForm from "./PasswordForm";
 
-export default function ProfileContent({profile, userPlan}) {
-    const panes = [
-        {menuItem: 'Plan', render: () => <AboutTab profile={profile} userPlan={userPlan} />},
-        {menuItem: 'Change my password', render: () => <PasswordForm />},
-    ]
-    return (
-        <Tab
-            menu={{fluid: true, vertical: true}} 
-            menuPosition='right'
-            panes={panes}
-        />
-    )
+export default function ProfileContent({ profile, userPlan }) {
+  const panes = [
+    {
+      menuItem: "Plan",
+      render: () => <AboutTab profile={profile} userPlan={userPlan} />,
+    },
+  ];
+  if (profile.providerType === "password")
+    panes.push({
+      menuItem: "Change my password",
+      render: () => <PasswordForm />,
+    });
+  return (
+    <Tab
+      menu={{ fluid: true, vertical: true }}
+      menuPosition="right"
+      panes={panes}
+    />
+  );
 }
